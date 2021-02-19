166569f0d45b23155de778bee79f452415a2e368
Guillaume Smet <guillaume.smet@gmail.com>
2021-02-19 10:39:43 +0100

    Move substitution to graal package, per convention

--- runtime/src/main/java/io/quarkiverse/freemarker/runtime/graal/Log4jOverSLF4JTesterSubstitute.java
+++ runtime/src/main/java/io/quarkiverse/freemarker/runtime/graal/Log4jOverSLF4JTesterSubstitute.java
@@ -1,4 +1,4 @@
-package io.quarkiverse.freemarker.runtime;
+package io.quarkiverse.freemarker.runtime.graal;
 
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
@@ -9,7 +9,7 @@ import freemarker.log._Log4jOverSLF4JTester;
  * avoid a class not found on org.apache.log4j.MDC
  */
 @TargetClass(_Log4jOverSLF4JTester.class)
-final class Log4jOverSLF4JTesterSubstitue {
+final class Log4jOverSLF4JTesterSubstitute {
 
     @Substitute
     public static final boolean test() {
